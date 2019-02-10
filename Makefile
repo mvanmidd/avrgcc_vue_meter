71481b305d1bfcd5d44cb71d83c86f25a8ef654c
markvan <mvanmiddle@gmail.com>
2019-02-10 16:04:10 -0500

    VUE meter demo with light_ws2812 library

--- Makefile
+++ Makefile
@@ -1,42 +1,53 @@
-# baud=115200
-avrType=atmega32u4
-avrFreq=16000000 # 16 Mhz
-# programmerDev=/dev/ttyUSB0
+# Makefile to build light_ws2812 library examples
+# This is not a very good example of a makefile - the dependencies do not work, therefore everything is rebuilt every time.
+
+# Change these parameters for your device
+
+F_CPU = 16000000
+DEVICE = atmega32u4
+
 programmerDev=`ls /*/cu.usb*`
 programmerType=avr109
-# programmerType=usbasp
 
-QMK_LIBS=-I/Users/markvan/devel/keyboard/qmk_firmware/drivers \
-						-I/Users/markvan/devel/keyboard/qmk_firmware/quantum
-ARDUINO_LIBS=-I/Applications/Arduino.app/Contents/Java/hardware/arduino/avr/cores/arduino \
-						-I/Applications/Arduino.app/Contents/Java/hardware/arduino/avr/variants/leonardo
-NEOPIXEL_LIBS=-I/Users/markvan/devel/keyboard/vumeter/Adafruit_NeoPixel
-LIGHTWS_LIBS=-I/Users/markvan/devel/keyboard/vumeter/light_ws2812
+# Tools:
+CC = avr-gcc
 
-cflags=-DF_CPU=$(avrFreq) -mmcu=$(avrType) -Wall -Werror -Wextra -Os \
-	-nostdlib -nostartfiles \
-	$(LIGHTWS_LIBS) \
-	$(QMK_LIBS)
+LIB       = light_ws2812
+# EXAMPLES  = RGB_blinky RGBW_blinky Chained_writes Rainbow
+EXAMPLES  = main
+# DEP		  = ws2812_config.h Light_WS2812/light_ws2812.h
+DEP		  = light_ws2812.h
+
+CFLAGS = -g2 -I. -ILight_WS2812 -mmcu=$(DEVICE) -DF_CPU=$(F_CPU)
+CFLAGS+= -Os -ffunction-sections -fdata-sections -fpack-struct -fno-move-loop-invariants -fno-tree-scev-cprop -fno-inline-small-functions
+CFLAGS+= -Wall -Wno-pointer-to-int-cast
+#CFLAGS+= -Wa,-ahls=$<.lst
+
+LDFLAGS = -Wl,--relax,--section-start=.text=0,-Map=main.map
+
+all:	$(EXAMPLES)
+# all: main.hex
 
 
-objects=$(patsubst %.c,%.o,$(wildcard *.c))
+$(LIB): $(DEP)
+	echo Building Library
+	$(CC) $(CFLAGS) -o $@.o -c $@.c
 
-.PHONY: flash clean
 
-all: main.hex
-
-%.o: %.c
-	avr-gcc $(cflags) -c $< -o $@
-
-main.elf: $(objects)
-	avr-gcc $(cflags) -o $@ $^
-
-main.hex: main.elf
-	avr-objcopy -j .text -j .data -O ihex $^ $@
+$(EXAMPLES): $(LIB)
+	echo Building $@
+	$(CC) $(CFLAGS) -o $@.o $@.c $^.c
+	avr-size $@.o
+	avr-objcopy -j .text  -j .data -O ihex $@.o $@.hex
+	avr-objdump -d -S $@.o >$@.lss
 
 flash: main.hex
-	# avrdude -p$(avrType) -c$(programmerType) -P$(programmerDev) -b$(baud) -v -U flash:w:$<
-	avrdude -p $(avrType) -c $(programmerType) -P $(programmerDev)  -v -U flash:w:$<
+	avrdude -p $(DEVICE) -c $(programmerType) -P $(programmerDev)  -v -U flash:w:$<
+
+
+
+
+.PHONY:	clean
 
 clean:
-	rm -f main.hex main.elf $(objects)
+	rm -f *.hex *.o *.lss
--- main.c
+++ main.c
@@ -1,17 +1,133 @@
 #include <util/delay.h>
 #include <avr/io.h>
 // #include <examples/strandtest/strandtest.ino>
+// #include <ws2812_config.h>
+#include <light_ws2812.h>
 
 // avr library from QMK firmware.
-#include <avr/ws2812.h>
+// #include <avr/ws2812.h>
 
-#define LED PORTB5
+// #define LED PORTB5
+
+#define MAXPIX  14
+
+#define COLORLENGTH (MAXPIX/2)
+#define FADE (256/COLORLENGTH)
+
+
+
+struct cRGB colors[8];
+struct cRGB led[MAXPIX];
+
+uint8_t j = 1;
+uint8_t k = 1;
+
+
+void setVue(int level)
+{
+  int colorLength = (MAXPIX/2);
+  int fadeLength = (256/colorLength);
+
+
+  DDRB|=_BV(ws2812_pin);
+
+
+  //Rainbowcolors
+  colors[0].r=150; colors[0].g=150; colors[0].b=150;
+  colors[1].r=255; colors[1].g=000; colors[1].b=000;//red
+  colors[2].r=255; colors[2].g=100; colors[2].b=000;//orange
+  colors[3].r=100; colors[3].g=255; colors[3].b=000;//yellow
+  colors[4].r=000; colors[4].g=255; colors[4].b=000;//green
+  colors[5].r=000; colors[5].g=100; colors[5].b=255;//light blue (türkis)
+  colors[6].r=000; colors[6].g=000; colors[6].b=255;//blue
+  colors[7].r=100; colors[7].g=000; colors[7].b=255;//violet
+
+  //shift all vallues by one led
+  uint8_t i;
+  for(i=MAXPIX; i>1; i--)
+      led[i-1]=led[i-2];
+  //change colour when colourlength is reached
+  if(k>COLORLENGTH)
+  {
+      j++;
+      if(j>7)
+      {
+        j=0;
+      }
+
+      k=0;
+  }
+  k++;
+  //loop colouers
+
+  //fade red
+  if(led[0].r<(colors[j].r-FADE))
+      led[0].r+=FADE;
+
+  if(led[0].r>(colors[j].r+FADE))
+      led[0].r-=FADE;
+
+  if(led[0].g<(colors[j].g-FADE))
+      led[0].g+=FADE;
+
+  if(led[0].g>(colors[j].g+FADE))
+      led[0].g-=FADE;
+
+  if(led[0].b<(colors[j].b-FADE))
+      led[0].b+=FADE;
+
+  if(led[0].b>(colors[j].b+FADE))
+      led[0].b-=FADE;
+
+  i = 0;
+  for(i=MAXPIX; i>level; i--)
+  {
+      led[i-1].r=0;led[i-1].g=0;led[i-1].b=0;
+  }
+
+  _delay_ms(10);
+  ws2812_sendarray((uint8_t *)led,MAXPIX*3);
+}
 
 int main(void) {
-  // setup();
-  // loop();
-  // int a = 0;
-  // for/*ever*/(;;) {
-  //   a += 1;
-  // }
+  int maxVue = 14;
+  int vueLevel = 2;
+  int steps = 0;
+  int maxSteps = 20;
+
+  uint8_t i;
+  for(i=MAXPIX; i>0; i--)
+  {
+      led[i-1].r=0;led[i-1].g=0;led[i-1].b=0;
+  }
+
+  while(1) {
+    setVue(vueLevel<=maxVue?vueLevel:(maxVue*2)-vueLevel);
+    steps += 1;
+    if (steps > maxSteps) {
+      vueLevel += 1;
+      vueLevel = vueLevel % (maxVue * 2);
+      steps = 0;
+    }
+
+  }
 }
+
+// struct cRGB led[1];
+//
+// int main(void) {
+//   while(1)
+//   {
+//     led[0].r=255;led[0].g=00;led[0].b=0;    // Write red to array
+//     ws2812_setleds(led,1);
+//     _delay_ms(500);                         // wait for 500ms.
+//
+//     led[0].r=0;led[0].g=255;led[0].b=0;			// green
+//     ws2812_setleds(led,1);
+//     _delay_ms(500);
+//
+//     led[0].r=0;led[0].g=00;led[0].b=255;		// blue
+//     ws2812_setleds(led,1);
+//     _delay_ms(500);
+//   }
+// }
